3ed6695241d730cdfc48be8959d84f6b328f0d02
dulce <7galvandulce@gmail.com>
2021-01-04 04:29:04 -0500

    added function for choices

--- Develop/script.js
+++ Develop/script.js
@@ -15,41 +15,81 @@ generateBtn.addEventListener("click", writePassword);
 
 //Variables 
 var passLenght;
-var uppercaseCheck
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "0123456789";
-var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
 var wantCharacters;
 var wantNumbers;
 var wantLowercase;
-var wantUppercase
+var wantUppercase;
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
 
-function writePassword(){
-   passLenght = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
+function writePassword() {
+  passLenght = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
 
-    if (passLenght<8){
-      alert("Password length must be a number between 8-128 characters");
-      writePassword()
+  if (passLenght < 8 || passLenght > 128 || (isNaN(passLenght))) {
+    alert("Password length must be a number between 8-128 characters");
+    writePassword()
 
-    } else if (passLenght>128){
+  } else if (passLenght > 8 || passLenght < 128)
 
-      alert("Password length must be between 8-128 characters");
-      writePassword()
+    wantCharacters = confirm("Do you want special characters?")
+  wantNumbers = confirm("Do you want numbers?")
+  wantUppercase = confirm("Do you want uppercase letter?")
+  wantLowercase = confirm("Do you want lowercase letters?")
 
-    } else if (isNaN(passLenght)){
 
-      alert("Password length must be between 8-128 characters");
-      writePassword()
-    } else { 
+  if (wantCharacters === false && wantNumbers === false && uppercase === false && wantLowercase === false) {
+    alert("You must select at least one character")
+    writePassword()
+  }
 
-      wantCharacters = confirm("Do you want special characters?")
-      wantNumbers = confirm("Do you want numbers?")
-      wantUppercase = confirm("Do you want uppercase letter?")
-      wantLowercase = confirm("Do you want lowercase letters?")
+  var storePassword = [];
+  var allCharacters = [];
+  var chosenCharacters = [];
 
-    }
+  if (wantCharacters) {
+    allCharacters = allCharacters.concat(specialCharacters);
+    chosenCharacters.push(getrandom(specialCharacters));
+
+    console.log(allCharacters)
+    console.log(chosenCharacters)
+  }
+
+  if (wantNumbers) {
+    allCharacters = allCharacters.concat(numbers);
+    chosenCharacters.push(getrandom(numbers));
+
+    console.log(allCharacters)
+    console.log(chosenCharacters)
+  }
+
+  if (wantUppercase) {
+    allCharacters = allCharacters.concat(uppercase);
+    chosenCharacters.push(getrandom(uppercase));
+
+    console.log(allCharacters)
+    console.log(chosenCharacters)
+  }
+
+  if (wantLowercase) {
+    allCharacters = allCharacters.concat(lowercase);
+    chosenCharacters.push(getrandom(lowercase));
+
+    console.log(allCharacters)
+    console.log(chosenCharacters)
+  }
 
 }
+
+function getrandom(arr) {
+
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  return randElement;
+
+}
+
+
 
 
